1dd5f143cf35805474214568dbe9f7cd4e55a18d
Gongas06 <goncaloferreira@pvc.pt>
2025-11-02 11:30:57 +0000

    Primeiro commit - projeto Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trabalho_Individual_1',
+  webDir: 'www'
+};
+
+export default config;
