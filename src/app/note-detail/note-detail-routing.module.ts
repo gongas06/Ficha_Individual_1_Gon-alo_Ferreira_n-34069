0fdd9d391fb97b316a7e9d2cb2deae1ec1bded1b
Gongas06 <goncaloferreira@pvc.pt>
2025-11-03 16:15:15 +0000

    Primeiro commit

--- src/app/note-detail/note-detail-routing.module.ts
+++ src/app/note-detail/note-detail-routing.module.ts
@@ -1,17 +1,1 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-import { NoteDetailPage } from './note-detail.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: NoteDetailPage
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class NoteDetailPageRoutingModule {}
--- src/app/note-detail/note-detail.page.ts
+++ src/app/note-detail/note-detail.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note-detail',
+  standalone: true,
   templateUrl: './note-detail.page.html',
   styleUrls: ['./note-detail.page.scss'],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class NoteDetailPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class NoteDetailPage {}
